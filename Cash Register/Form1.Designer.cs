e2228b2195392f0e7d5cb9f8975cac5a2ea6ffdd
Ethan Hillier <ethahill104@ed.amdsb.ca>
2020-02-24 13:11:21 -0500

    Add project files.

---
+++ Cash Register/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Cash_Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.subTotal = new System.Windows.Forms.Label();
+            this.Tax = new System.Windows.Forms.Label();
+            this.TotalCost = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tendered1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.changeBox = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.print = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(72, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(72, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(47, 117);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(239, 23);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // subTotal
+            // 
+            this.subTotal.AutoSize = true;
+            this.subTotal.Location = new System.Drawing.Point(111, 161);
+            this.subTotal.Name = "subTotal";
+            this.subTotal.Size = new System.Drawing.Size(53, 13);
+            this.subTotal.TabIndex = 7;
+            this.subTotal.Text = "SubTotal:";
+            // 
+            // Tax
+            // 
+            this.Tax.AutoSize = true;
+            this.Tax.Location = new System.Drawing.Point(111, 187);
+            this.Tax.Name = "Tax";
+            this.Tax.Size = new System.Drawing.Size(28, 13);
+            this.Tax.TabIndex = 8;
+            this.Tax.Text = "Tax:";
+            // 
+            // TotalCost
+            // 
+            this.TotalCost.AutoSize = true;
+            this.TotalCost.Location = new System.Drawing.Point(111, 209);
+            this.TotalCost.Name = "TotalCost";
+            this.TotalCost.Size = new System.Drawing.Size(34, 13);
+            this.TotalCost.TabIndex = 9;
+            this.TotalCost.Text = "Total:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(72, 81);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(214, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // tendered1
+            // 
+            this.tendered1.AutoSize = true;
+            this.tendered1.Location = new System.Drawing.Point(111, 235);
+            this.tendered1.Name = "tendered1";
+            this.tendered1.Size = new System.Drawing.Size(53, 13);
+            this.tendered1.TabIndex = 12;
+            this.tendered1.Text = "Tendered";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(201, 232);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(130, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // changeBox
+            // 
+            this.changeBox.AutoSize = true;
+            this.changeBox.Location = new System.Drawing.Point(114, 290);
+            this.changeBox.Name = "changeBox";
+            this.changeBox.Size = new System.Drawing.Size(44, 13);
+            this.changeBox.TabIndex = 14;
+            this.changeBox.Text = "Change";
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(144, 258);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(161, 23);
+            this.changeButton.TabIndex = 15;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(117, 323);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(214, 23);
+            this.print.TabIndex = 16;
+            this.print.Text = "Print";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.Print_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(568, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(404, 62);
+            this.label6.MinimumSize = new System.Drawing.Size(380, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(380, 290);
+            this.label6.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.changeBox);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.tendered1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TotalCost);
+            this.Controls.Add(this.Tax);
+            this.Controls.Add(this.subTotal);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label subTotal;
+        private System.Windows.Forms.Label Tax;
+        private System.Windows.Forms.Label TotalCost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label tendered1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label changeBox;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Cash Register/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cash_Register
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        double totalCostOfOrder;
+        double totalCost;
+        const double tax = 0.13;
+        double taxTotal;
+        double change;
+        double tendered;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+           
+
+            const double itemPrice1 = 999; //price of item 1
+            const double itemPrice2 = 220; //price of item 2
+            const double itemPrice3 = 49;//price of item 3
+            const double itemPrice4 = 19;//price of item 4
+
+            double numberOf1 = Convert.ToInt16(textBox1.Text);
+            double numberOf2 = Convert.ToInt16(textBox3.Text);
+            double numberOf3 = Convert.ToInt16(textBox2.Text);
+            double numberOf4 = Convert.ToInt16(textBox4.Text);
+          
+
+
+            totalCostOfOrder = itemPrice1 * numberOf1 + itemPrice2 * numberOf2 + itemPrice3 * numberOf3 + itemPrice4 * numberOf4;
+            totalCost = totalCostOfOrder + tax;
+            taxTotal = totalCostOfOrder * tax;
+
+            subTotal.Text = "Sub Total $" + totalCostOfOrder;
+            Tax.Text = "Tax $" + taxTotal;
+            TotalCost.Text = "Total $" + totalCost;
+
+
+
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            double tendered = Convert.ToDouble(textBox5.Text);
+            change = tendered - totalCost;
+
+            changeBox.Text = "Change $" + change; 
+
+        }
+
+        private void Print_Click(object sender, EventArgs e)
+        {
+           
+            
+
+            const double itemPrice1 = 999; //price of item 1
+            const double itemPrice2 = 220; //price of item 2
+            const double itemPrice3 = 49;//price of item 3
+            const double itemPrice4 = 19;//price of item 4
+
+            double numberOf1 = Convert.ToInt16(textBox1.Text);
+            double numberOf2 = Convert.ToInt16(textBox3.Text);
+            double numberOf3 = Convert.ToInt16(textBox2.Text);
+            double numberOf4 = Convert.ToInt16(textBox4.Text);
+           
+
+
+            double totalCostOfOrder;
+            totalCostOfOrder = itemPrice1 * numberOf1 + itemPrice2 * numberOf2 + itemPrice3 * numberOf3 + itemPrice4 * numberOf4;
+            totalCost = totalCostOfOrder + tax;
+            taxTotal = totalCostOfOrder * tax;
+
+            label5.Text = "Receipt";
+
+            label6.Text += "Sub Total:  " + totalCostOfOrder + "\n";
+            label6.Text += "Tax:  " + taxTotal + "\n";
+            label6.Text += "Total:  " + totalCost + "\n";
+            label6.Text += "Tendered:  " + tendered + "\n";
+            label6.Text += "Change  " + change + "\n";
+
+        }
+    }
+}
